e3b443f8c7f89ad037150de06917a80732d85547
Jake Walklate <jakewalklate@protonmail.com>
2025-12-29 21:45:06 +1100

    new

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import js from '@eslint/js'
-import globals from './new/node_modules/globals'
-import reactHooks from './new/node_modules/eslint-plugin-react-hooks'
-import reactRefresh from './new/node_modules/eslint-plugin-react-refresh'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
